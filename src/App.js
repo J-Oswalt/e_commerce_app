24386239d7cf925607b4352335883b11a6c0a4a4
J-Oswalt <josephoswalt95@gmail.com>
2024-04-13 11:32:00 -0400

    Added the backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import ProductsListPage from "./pages/ProductsListPage";
+import categories from "./categories";
+import users from "./users";
+import LoginPage from "./pages/LoginPage";
 
-function App() {
+const App = () => {
+  const loggedIdUser = JSON.parse(sessionStorage.getItem('loggedInUser'))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          {loggedIdUser && <ProductsListPage />}
+          {!loggedIdUser && <LoginPage />}
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
